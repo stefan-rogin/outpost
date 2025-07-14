f752e0b24f5767968e7d1fb07bb84e5d1c1d5c47
stefan-rogin <stefan.rogin@outlook.com>
2025-07-14 11:02:37 +0000

    selective bom

--- src/components/BoM.tsx
+++ src/components/BoM.tsx
@@ -1,33 +1,130 @@
-import { ResourceId } from "@/models/resource"
+import { ResourceId, Resource, isConstructible } from "@/models/resource"
 import { Order } from "@/models/order"
 import styles from "@/components/BoM.module.css"
 import { getResource } from "@/lib/resources"
+import { useState, useMemo, useEffect } from "react"
+
+export interface BomItem {
+  item: Resource
+  quantity: number
+}
+
+export type Bill = Map<ResourceId, BomItem>
 
 export const BoM = ({ order }: { order: Order }) => {
-  const bom: Map<ResourceId, number> = aggregateBlueprints(order)
+  const [deconstructed, setDeconstructed] = useState<Bill>(new Map())
+  const bom = useMemo(
+    () => aggregateBlueprints(order, deconstructed),
+    [order, deconstructed]
+  )
+
+  const isDeconstructed = (id: ResourceId) => deconstructed.has(id)
+
+  const toggleDeconstructed = (bomItem: BomItem) => (): void => {
+    if (!isConstructible(bomItem.item)) return
+    const id = bomItem.item.id
+    const newDeconstructed = new Map<ResourceId, BomItem>(deconstructed)
+    deconstructed.has(id)
+      ? newDeconstructed.delete(id)
+      : newDeconstructed.set(id, bomItem)
+    console.log(newDeconstructed)
+    setDeconstructed(newDeconstructed)
+  }
 
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>Materials</h3>
       <div className={styles.bom}>
-        {[...bom].map(([id, qty]) => {
-          return (
-            <div className={styles.bom_item} key={id}>
-              {qty} x {getResource(id)?.name ?? id}
-            </div>
-          )
-        })}
+        {[...deconstructed.values(), ...bom.values()]
+          .sort(compareFn)
+          .map(bomItem => {
+            const className = `${styles.bom_item} ${
+              isConstructible(bomItem.item) ? styles.bom_constructible : ""
+            } ${
+              isDeconstructed(bomItem.item.id) ? styles.bom_deconstructed : ""
+            }`
+            return (
+              <div
+                className={className}
+                key={bomItem.item.id}
+                onClick={toggleDeconstructed(bomItem)}
+              >
+                {bomItem.quantity} x {bomItem.item.name ?? bomItem.item.id}
+              </div>
+            )
+          })}
       </div>
     </div>
   )
 }
 
-export function aggregateBlueprints(order: Order): Map<ResourceId, number> {
-  const result = new Map<ResourceId, number>()
+export function compareFn(a: BomItem, b: BomItem) {
+  // TODO: Introduce tiers/scarcity
+  if (isConstructible(a.item) && !isConstructible(b.item)) return -1
+  else if (!isConstructible(b.item) && !isConstructible(a.item)) return 1
+  else return 0
+}
 
+export function aggregateBlueprints(order: Order, deconstructed: Bill): Bill {
+  const result = new Map<ResourceId, BomItem>()
   for (const orderItem of order.values()) {
-    for (const [bpId, bpQty] of Object.entries(orderItem.item.blueprint)) {
-      result.set(bpId, (result.get(bpId) || 0) + orderItem.quantity * bpQty)
+    const itemBom = getBomForInput(
+      new Map(),
+      orderItem.item.id,
+      orderItem.quantity,
+      deconstructed,
+      true
+    )
+    for (const bomResource of itemBom.values()) {
+      mergeResource(result, bomResource)
+    }
+  }
+  return result
+}
+
+export function mergeResource(prev: Bill, newResource: BomItem) {
+  const id = newResource.item.id
+  const existing = prev.get(id)
+  prev.set(id, {
+    item: newResource.item,
+    quantity: (existing?.quantity || 0) + newResource.quantity
+  })
+}
+
+export function getBomForInput(
+  prev: Bill,
+  id: ResourceId,
+  qty: number,
+  deconstructed: Bill,
+  isOrderItem = false
+): Bill {
+  const result = new Map(prev)
+  const resource = getResource(id)
+
+  if (resource == undefined) return result
+
+  if (
+    !isConstructible(resource) ||
+    (!deconstructed.has(resource.id) && !isOrderItem)
+  ) {
+    mergeResource(result, {
+      item: resource,
+      quantity: qty
+    })
+    return result
+  }
+
+  for (const [blueprintId, blueprintQty] of Object.entries(
+    resource.blueprint
+  )) {
+    const partial = getBomForInput(
+      new Map(),
+      blueprintId,
+      blueprintQty * qty,
+      deconstructed
+    )
+    for (const resourceFromBlueprint of partial.values()) {
+      mergeResource(result, resourceFromBlueprint)
     }
   }
 
--- tests/components/BoM.test.tsx
+++ tests/components/BoM.test.tsx
@@ -1,13 +1,37 @@
 import { ResourceId, Resource } from "@/models/resource"
 import * as Resources from "@/lib/resources"
-import { aggregateBlueprints } from "@/components/BoM"
+import { aggregateBlueprints, BomItem, Bill } from "@/components/BoM"
 import { testResources, testOrder } from "../testObjects"
 
-const expected = new Map<ResourceId, number>([
-  ["Mfg_Tier01_AdaptiveFrame", 2 * 10 + 3 * 10],
-  ["InorgCommonAluminum", 2 * 20 + 3 * 16],
-  ["InorgCommonIron", 3 * 20],
-  ["InorgCommonNickel", 2 * 16]
+const expected: Bill = new Map<ResourceId, BomItem>([
+  [
+    "Mfg_Tier01_AdaptiveFrame",
+    {
+      item: testResources["Mfg_Tier01_AdaptiveFrame"],
+      quantity: 2 * 10 + 3 * 10
+    }
+  ],
+  [
+    "InorgCommonAluminum",
+    {
+      item: testResources["InorgCommonAluminum"],
+      quantity: 2 * 20 + 3 * 16
+    }
+  ],
+  [
+    "InorgCommonIron",
+    {
+      item: testResources["InorgCommonIron"],
+      quantity: 3 * 20
+    }
+  ],
+  [
+    "InorgCommonNickel",
+    {
+      item: testResources["InorgCommonNickel"],
+      quantity: 2 * 16
+    }
+  ]
 ])
 
 jest.mock("@/lib/resources", () => ({
@@ -20,6 +44,6 @@ getResource.mockImplementation(id => testResources[id])
 
 describe("BoM tests", () => {
   test("aggregates materials for a list of constructible items", () => {
-    expect(aggregateBlueprints(testOrder)).toStrictEqual(expected)
+    expect(aggregateBlueprints(testOrder, new Map())).toStrictEqual(expected)
   })
 })
